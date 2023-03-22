48144b5db016563679311afd102cf60ba1a9cfe6
Leif Arriens <leif.arriens@gmail.com>
2023-03-22 22:10:12 +0100

    api json response

--- api/index.go
+++ api/index.go
@@ -1,13 +1,27 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/golang-module/carbon/v2"
 )
 
+type Message struct {
+	Time string
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
 	now := carbon.Now().ToString()
-	fmt.Fprintf(w, "<h1>Hello from Go! %s</h1>", now)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	m := Message{now}
+	b, err := json.Marshal(m)
+
+	if err != nil {
+		http.Error(w, fmt.Sprintf("error getting now, %v", err), http.StatusInternalServerError)
+	}
+
+	fmt.Fprintf(w, "%s", b)
 }
